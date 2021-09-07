c4ec0287ca318bb6c6a14bbdc4274dddc940f1cf
dinar <1910631170157@student.unsika.ac.id>
2021-09-07 23:16:09 +0700

    kamar

---
+++ admin/pages/kamar/jenis_order.php
@@ -0,0 +1,85 @@
+
+
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Kamar</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="<?= $admin_url ?>main.php?pages=home">Home</a></li>
+              <li class="breadcrumb-item active">Kamar</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+<div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Daftar Kamar</h3>
+
+                <div class="card-tools">
+                  <ul class="pagination pagination-sm float-right">
+                    <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                  </ul>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th style="width: 20px">#</th>
+                      <th>Nama Kamar</th>
+                      <th>Aksi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                            include "../lib/config.php";
+                            include "../lib/koneksi.php";
+                            $query = mysqli_query($koneksi, "SELECT * FROM tb_kamar");
+                            $i=1;
+                            while($oj=mysqli_fetch_array($query)){                              
+                          ?>
+                    <tr>
+                      <td><?= $i ?>.</td>
+                      <td><?= $oj['jenis_order'] ?></td>
+                      <td>
+                            <div class="input-group-btn">
+                              <a href="<?= $admin_url; ?>main.php?pages=edit_kamar&id_kamar=<?= $oj['id_kamar']; ?>" class="btn btn-warning"><i class="fas fa-edit"></i></a>
+                              <a href="<?= $admin_url; ?>pages/kamar/aksi_hapus.php?id_kamar=<?= $oj['id_kamar']; ?>" class="btn btn-danger"><i class="fas fa-power-off"></i></a>
+                            </div>
+                          </td>
+                    </tr>
+                    <?php $i++;} ?> 
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <ul class="nav navbar-right panel_toolbox">
+                      <div class="input-group-btn float-right">
+                      <a href="main.php?pages=tambah_kamar">
+                        <button href="index.php" type="button" class="btn btn-primary">Tambah Daftar</button>
+                      </a>
+                    </div>                      
+                    </ul>
+            <!-- /.card -->
+                    </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
