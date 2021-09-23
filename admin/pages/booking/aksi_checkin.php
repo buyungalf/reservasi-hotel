df68e50ddfc090792d3fb449a6a689fda3aac9fd
unknown <buyungalf@gmail.com>
2021-09-23 12:48:18 +0700

    menu booking

---
+++ admin/pages/booking/aksi_checkin.php
@@ -0,0 +1,40 @@
+<?php 
+session_start();
+if (empty($_SESSION['username']) AND empty($_SESSION['passuser'])) {
+  echo "<center>Untuk mengakses modul, Anda harus login dulu <br>";
+  echo "<a href=../../index.php><b>LOGIN</b></a></center>";
+} else {
+	include "../../../lib/config.php";
+	include "../../../lib/koneksi.php";
+
+	$id_pesan = $_POST['id_pesan'];
+	$no_pesan = $_POST['no_pesan'];
+	$id_kamar = $_POST['id_kamar'];
+	$no_trans = $_POST['no_trans'];
+	$nm_tamu = $_POST['nama'];
+	$alamat = $_POST['alamat'];
+	$telp = $_POST['phone'];
+	$identitas = $_POST['identitas'];
+	$no_id = $_POST['no_identitas'];
+	$keterangan = $_POST['keterangan'];
+	$checkin = mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y"));
+
+	$querySimpan = mysqli_query($koneksi,"INSERT INTO tb_tamu(id_kamar, no_trans, nm_tamu, alamat, telp, identitas, no_id, keterangan, diskon, checkin) VALUES ('$id_kamar', '$no_trans', '$nm_tamu', '$alamat', '$telp', '$identitas', '$no_id', '$keterangan', '$disc', '$checkin')");
+	if($querySimpan) {
+		$queryUpdate = mysqli_query($koneksi, "UPDATE tb_kamar set status='use' where id_kamar='$id_kamar'");
+			if($queryUpdate) {
+				$queryHapus1 = mysqli_query($koneksi, "DELETE from tb_pesan_kamar where no_pesan = '$no_pesan'");
+				if($queryHapus1) {
+					$queryHapus2 = mysqli_query($koneksi, "DELETE from tb_pesan where no_pesan = '$no_pesan'");
+					echo"<script> alert('Check in Berhasil!'); window.location = '$admin_url'+'main.php?pages=reservasi'; </script>"; 
+				} else {
+					echo "<script> alert('gagal 1'); window.location = '$admin_url'+'main.php?pages=booking_detail&id_booking=$id_pesan';</script>";
+				}
+			} else {
+					echo "<script> alert('gagal 2'); window.location = '$admin_url'+'main.php?pages=booking_detail&id_booking=$id_pesan';</script>";
+			}
+		} else {
+			echo "<script> alert('Data Gagal Masuk'); window.location = '$admin_url'+'main.php?pages=booking_detail&id_booking=$id_pesan';</script>";
+		}
+	}				
+?>
---
+++ admin/pages/booking/booking_detail.php
@@ -0,0 +1,144 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Reservasi</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="<?= $admin_url ?>main.php?pages=home">Home</a></li>
+              <li class="breadcrumb-item"><a href="<?= $admin_url ?>main.php?pages=reservasi">Reservasi</a></li>
+              <li class="breadcrumb-item active">Shop</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <div class="col-12">
+<!-- Horizontal Form -->
+            <form method="post" action="pages/booking/aksi_checkin.php" class="form-horizontal">
+            <div class="card card-info">  
+            <div class="card-header">
+              <h3 class="card-title">Daftar Kamar Pesanan</h3>
+            </div>           
+              <!-- /.card-header -->
+                <?php                  
+                  include "../lib/config.php";
+                  include "../lib/koneksi.php";
+                  $id_booking = $_GET['id_booking'];
+                  $query = mysqli_query($koneksi, "SELECT * from tb_pesan d join tb_pesan_kamar a
+                                                  on d.no_pesan = a.no_pesan join tb_kamar b 
+                                                  on a.id_kamar = b.id_kamar 
+                                                  join tb_kamar_tipe c 
+                                                  on b.tipe = c.id_tipe WHERE id_pesan= $id_booking");
+                  $bo=mysqli_fetch_array($query);
+                  $tgl = mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y")); 
+                  $no_trans = date("00jnYHis",$tgl);
+                  $i=1;                           
+                ?>
+                <div class="card-body">
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">No. Pemesanan</label>
+                    <div class="col-sm-10">
+                      <label class="col-form-label"><?= $bo['no_pesan'] ?></label>
+                      <input type="hidden" name="no_pesan" value="<?= $bo['no_pesan'] ?>">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">No. Transaksi</label>
+                    <div class="col-sm-10">
+                      <label class="col-form-label">CI/<?= $no_trans ?></label>
+                      <input type="hidden" name="no_trans" value="CI/00<?= $no_trans ?>">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Nama Kamar</label>
+                    <div class="col-sm-10">
+                      <label class="col-form-label"><?= $bo['nm_kamar'] ?></label>
+                      <input type="hidden" name="id_kamar" value="<?= $bo['id_kamar'] ?>">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Waktu Check-in</label>
+                    <div class="col-sm-10">
+                      <label class="col-form-label"><?= date("j/n/Y | H:i",$bo['tgl_pesan']); ?></label>
+                      <input type="hidden" name="id_pesan" value="<?= $bo['id_pesan'] ?>">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Nama Pemesan</label>
+                    <div class="col-sm-10">
+                      <label class="col-form-label"><?= $bo['nama'] ?></label>
+                      <input type="hidden" name="nama" value="<?= $bo['nama'] ?>">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Identitas</label>
+                    <div class="col-sm-10">
+                      <select class="form-control select2" name="identitas" style="width: 100%;">
+                        <option value="">-- Pilih Kartu Identitas --</option>
+                        <?php
+                          include "../lib/config.php";
+                          include "../lib/koneksi.php";
+                          $q = mysqli_query($koneksi, "SELECT * FROM tb_identitas");
+                          while($id=mysqli_fetch_array($q)){
+                        ?>                          
+                          <option value="<?php echo $id['id_jenis']; ?>">
+                            <?php echo $id['jenis']; ?>
+                          </option>
+                          <?php } ?>
+                      </select>
+                    </div>
+                  </div> 
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">No. Identitas</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" name="no_identitas" placeholder="No. Identitas">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Alamat</label>
+                    <div class="col-sm-10">
+                      <label class="col-form-label"><?= $bo['alamat'] ?></label>
+                      <input type="hidden" name="alamat" value="<?= $bo['alamat'] ?>">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Phone</label>
+                    <div class="col-sm-10">
+                      <label class="col-form-label"><?= $bo['phone'] ?></label>
+                      <input type="hidden" name="phone" value="<?= $bo['email'] ?>">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Disc</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" name="disc" placeholder="Discount">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Keterangan</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" name="keterangan" placeholder="Keterangan">
+                    </div>
+                  </div>
+                </div>         
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button class="btn btn-default float-right ml-2">Batal</button>
+                  <button type="submit" class="btn btn-info float-right">Tambah</button>                  
+                </div>
+                <!-- /.card-footer -->
+              </form>
+              </div>              
+            </div>
+            <!-- /.card -->
+                    </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
